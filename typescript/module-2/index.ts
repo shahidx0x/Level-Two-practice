47680edbc77a3ffddb0702fa68c4cf277c1c66db
shahidx0x <shahid.dev.cse@gmail.com>
2023-05-07 23:44:14 +0600

    Auto-commit: 406

--- typescript/module-2/index.ts
+++ typescript/module-2/index.ts
@@ -131,12 +131,21 @@ applay_korlam(userData);
 
 
 function getArrayItem<T, K extends keyof T>(arr: T[], index: number, key: K): T[K] {
-
     const item = arr[index];
-  
     return item[key];
   
   }
   
   const users = [ { name: 'John', age: 30 }, { name: 'Mary', age: 25 },];
   console.log(getArrayItem(users, 0, 'name'))
+
+
+  interface Person {
+    firstName: string;
+    lastName: string;
+  }
+
+  function fullName<T extends Person>(person: T): string {
+    return `${person.firstName} ${person.lastName}`;
+}
+  
